797cc8cf41cd29549adc7b2cba15f229059ab0c8
ewancoder <ewancoder@gmail.com>
2014-03-13 01:10:12 +0300

    Updated install script

---
+++ al.sh
@@ -0,0 +1,17 @@
+clear
+echo Downloading Ewancoder Arch Linux files for installation...
+echo -e "\n-> eal.sh"
+curl -O https://raw.github.com/ewancoder/dotfiles/master/.eal/eal.sh
+echo -e "\n-> eal-chroot.sh"
+curl -O https://raw.github.com/ewancoder/dotfiles/master/.eal/eal-chroot.sh
+echo -e "\n-> ceal.sh"
+curl -O https://raw.github.com/ewancoder/dotfiles/master/.install/ceal.sh
+echo -e "\n-> peal.sh"
+curl -O https://raw.github.com/ewancoder/dotfiles/master/.install/peal.sh
+echo -e "\n-> peal-user.sh"
+curl -O https://raw.github.com/ewancoder/dotfiles/master/.install/peal-user.sh
+echo -e "\n-> peal-merge.sh"
+curl -O https://raw.github.com/ewancoder/dotfiles/master/.install/peal-merge.sh
+echo -e "\nDownload complete. Making scripts executable...\n"
+chmod +x {eal,peal}*.sh
+echo -e "Now execute ./eal.sh to install or edit it before installation. Also don\'t forget to mount partitions before installation and change constants in ceal.sh file.\n"
--- archinstall.sh
+++
@@ -1,3 +0,0 @@
-curl -O https://raw.github.com/ewancoder/dotfiles/master/.install/install.sh
-chmod +x install.sh
-./install.sh
