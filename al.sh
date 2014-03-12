03f146642261ede83ab40133e4128dcfecdeb574
ewancoder <ewancoder@gmail.com>
2014-03-13 01:14:39 +0300

    fixed issue

--- al.sh
+++ al.sh
@@ -5,13 +5,13 @@ curl -O https://raw.github.com/ewancoder/dotfiles/master/.eal/eal.sh
 echo -e "\n-> eal-chroot.sh"
 curl -O https://raw.github.com/ewancoder/dotfiles/master/.eal/eal-chroot.sh
 echo -e "\n-> ceal.sh"
-curl -O https://raw.github.com/ewancoder/dotfiles/master/.install/ceal.sh
+curl -O https://raw.github.com/ewancoder/dotfiles/master/.eal/ceal.sh
 echo -e "\n-> peal.sh"
-curl -O https://raw.github.com/ewancoder/dotfiles/master/.install/peal.sh
+curl -O https://raw.github.com/ewancoder/dotfiles/master/.eal/peal.sh
 echo -e "\n-> peal-user.sh"
-curl -O https://raw.github.com/ewancoder/dotfiles/master/.install/peal-user.sh
+curl -O https://raw.github.com/ewancoder/dotfiles/master/.eal/peal-user.sh
 echo -e "\n-> peal-merge.sh"
-curl -O https://raw.github.com/ewancoder/dotfiles/master/.install/peal-merge.sh
+curl -O https://raw.github.com/ewancoder/dotfiles/master/.eal/peal-merge.sh
 echo -e "\nDownload complete. Making scripts executable...\n"
 chmod +x {eal,peal}*.sh
 echo -e "Now execute ./eal.sh to install or edit it before installation. Also don\'t forget to mount partitions before installation and change constants in ceal.sh file.\n"
