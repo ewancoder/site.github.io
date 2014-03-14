3e59d7c113fffee20b96c1d93ce44f7fa1da9dbc
ewancoder <ewancoder@gmail.com>
2014-03-15 00:13:41 +0300

    fixed \' -> ' issue'

--- al.sh
+++ al.sh
@@ -14,4 +14,4 @@ echo -e "\n-> peal-merge.sh"
 curl -O https://raw.github.com/ewancoder/dotfiles/master/.eal/peal-merge.sh
 echo -e "\nDownload complete. Making scripts executable...\n"
 chmod +x {eal,peal}*.sh
-echo -e "Now execute ./eal.sh to install or edit it before installation. Also don\'t forget to mount partitions before installation and change constants in ceal.sh file.\n"
+echo -e "Now execute ./eal.sh to install or edit it before installation. Also don't forget to mount partitions before installation and change constants in ceal.sh file.\n"
