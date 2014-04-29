d5b91af5461922e852409e3e4b23b76fcac5fb1e
Ewan <lanceulot@gmail.com>
2014-04-29 15:03:16 +0300

    Fixed raw github -> githubusercontent
    I assume, this is needed with new github update - now it's correct path to raw files on github.

--- al.sh
+++ al.sh
@@ -1,17 +1,17 @@
 clear
 echo Downloading Ewancoder Arch Linux files for installation...
 echo -e "\n-> eal.sh"
-curl -O https://raw.github.com/ewancoder/dotfiles/master/.eal/eal.sh
+curl -O https://raw.githubusercontent.com/ewancoder/dotfiles/master/.eal/eal.sh
 echo -e "\n-> eal-chroot.sh"
-curl -O https://raw.github.com/ewancoder/dotfiles/master/.eal/eal-chroot.sh
+curl -O https://raw.githubusercontent.com/ewancoder/dotfiles/master/.eal/eal-chroot.sh
 echo -e "\n-> ceal.sh"
-curl -O https://raw.github.com/ewancoder/dotfiles/master/.eal/ceal.sh
+curl -O https://raw.githubusercontent.com/ewancoder/dotfiles/master/.eal/ceal.sh
 echo -e "\n-> peal.sh"
-curl -O https://raw.github.com/ewancoder/dotfiles/master/.eal/peal.sh
+curl -O https://raw.githubusercontent.com/ewancoder/dotfiles/master/.eal/peal.sh
 echo -e "\n-> peal-user.sh"
-curl -O https://raw.github.com/ewancoder/dotfiles/master/.eal/peal-user.sh
+curl -O https://raw.githubusercontent.com/ewancoder/dotfiles/master/.eal/peal-user.sh
 echo -e "\n-> peal-merge.sh"
-curl -O https://raw.github.com/ewancoder/dotfiles/master/.eal/peal-merge.sh
+curl -O https://raw.githubusercontent.com/ewancoder/dotfiles/master/.eal/peal-merge.sh
 echo -e "\nDownload complete. Making scripts executable...\n"
 chmod +x {eal,peal}*.sh
 echo -e "Now execute ./eal.sh to install or edit it before installation. Also don't forget to mount partitions before installation and change constants in ceal.sh file.\n"
