dc6439aaa35bc7801abd66992185cc81a7d7c04a
ewancoder <ewancoder@gmail.com>
2014-06-07 12:06:44 +0300

    Fixed al.sh based on new repo

--- al.sh
+++ al.sh
@@ -1,17 +1,11 @@
 clear
 echo Downloading Ewancoder Arch Linux files for installation...
 echo -e "\n-> eal.sh"
-curl -O https://raw.githubusercontent.com/ewancoder/dotfiles/master/.eal/eal.sh
-echo -e "\n-> eal-chroot.sh"
-curl -O https://raw.githubusercontent.com/ewancoder/dotfiles/master/.eal/eal-chroot.sh
+curl -O https://raw.githubusercontent.com/ewancoder/eal/master/eal.sh
 echo -e "\n-> ceal.sh"
-curl -O https://raw.githubusercontent.com/ewancoder/dotfiles/master/.eal/ceal.sh
+curl -O https://raw.githubusercontent.com/ewancoder/eal/master/ceal.sh
 echo -e "\n-> peal.sh"
-curl -O https://raw.githubusercontent.com/ewancoder/dotfiles/master/.eal/peal.sh
-echo -e "\n-> peal-user.sh"
-curl -O https://raw.githubusercontent.com/ewancoder/dotfiles/master/.eal/peal-user.sh
-echo -e "\n-> peal-merge.sh"
-curl -O https://raw.githubusercontent.com/ewancoder/dotfiles/master/.eal/peal-merge.sh
+curl -O https://raw.githubusercontent.com/ewancoder/eal/master/peal.sh
 echo -e "\nDownload complete. Making scripts executable...\n"
 chmod +x {eal,peal}*.sh
 echo -e "Now execute ./eal.sh to install or edit it before installation. Also don't forget to mount partitions before installation and change constants in ceal.sh file.\n"
