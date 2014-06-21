01aa9f68fd89a7d5fe0211c406f012c3b4c7ff04
ewancoder <ewancoder@gmail.com>
2014-06-21 04:36:05 +0300

    Changed al.sh for corresponding version

--- al.sh
+++ al.sh
@@ -1,11 +1,15 @@
 clear
-echo Downloading Ewancoder Arch Linux files for installation...
+echo Downloading Ewancoder Arch Linux script files for installation...
 echo -e "\n-> eal.sh"
 curl -O https://raw.githubusercontent.com/ewancoder/eal/master/eal.sh
 echo -e "\n-> ceal.sh"
 curl -O https://raw.githubusercontent.com/ewancoder/eal/master/ceal.sh
+echo -e "\n-> heal.sh"
+curl -O https://raw.githubusercontent.com/ewancoder/eal/master/heal.sh
 echo -e "\n-> peal.sh"
 curl -O https://raw.githubusercontent.com/ewancoder/eal/master/peal.sh
-echo -e "\nDownload complete. Making scripts executable...\n"
-chmod +x {eal,peal}*.sh
-echo -e "Now execute ./eal.sh to install or edit it before installation. Also don't forget to mount partitions before installation and change constants in ceal.sh file.\n"
+echo -e "\n-> install.sh"
+curl -O https://raw.githubusercontent.com/ewancoder/eal/master/install.sh
+echo -e "\nDownload complete. Making install.sh executable...\n"
+chmod +x install.sh
+echo -e "Now prepare your partitions, change all constants in ceal.sh file and execute ./install.sh to start installation\n"
